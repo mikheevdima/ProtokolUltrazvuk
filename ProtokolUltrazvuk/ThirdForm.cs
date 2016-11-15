bd00726f1f47c0f12dde460037d66e4aaeb9efda
Dmitrii <micheev.dima@gmail.com>
2016-11-16 01:38:47 +0300

    added test

--- ProtokolUltrazvuk/ThirdForm.cs
+++ ProtokolUltrazvuk/ThirdForm.cs
@@ -7,12 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using word = Microsoft.Office.Interop.Word;
 
 namespace ProtokolUltrazvuk
 {
     public partial class ThirdForm : Form
     {
         Random rand = new Random();
+
+        public static void ExportToWord(DataGridView dgv)
+        {
+            MessageBox.Show("Opening Word");
+
+            word.Application word = null;
+
+
+
+            word.Document doc = null;
+            object oMissing = System.Reflection.Missing.Value;
+            object oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
+            try
+            {
+                word = new word.Application();
+                word.Visible = true;
+                doc = word.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+            }
+            if (word != null && doc != null)
+            {
+                word.Table newTable;
+                word.Range wrdRng = doc.Bookmarks.get_Item(ref oEndOfDoc).Range;
+                newTable = doc.Tables.Add(wrdRng, 1, dgv.Columns.Count - 1, ref oMissing, ref oMissing);
+                newTable.Borders.InsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+                newTable.Borders.OutsideLineStyle = Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+                newTable.AllowAutoFit = true;
+
+                foreach (DataGridViewCell cell in dgv.Rows[0].Cells)
+                {
+                    newTable.Cell(newTable.Rows.Count, cell.ColumnIndex).Range.Text = dgv.Columns[cell.ColumnIndex].Name;
+
+                }
+                newTable.Rows.Add();
+
+                foreach (DataGridViewRow row in dgv.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        newTable.Cell(newTable.Rows.Count, cell.ColumnIndex).Range.Text = cell.Value.ToString();
+                    }
+                    newTable.Rows.Add();
+                }
+            }
+
+        }
 
         public ThirdForm()
         {
@@ -137,6 +190,10 @@ namespace ProtokolUltrazvuk
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+
+
+            
             string[,] array =
             {
                 {"первое", "vtor", "tret", "chet"},
@@ -156,12 +213,12 @@ namespace ProtokolUltrazvuk
                 }
             }
 
+                        ExportToWord(dataGridView1);
 
-
-
+            /*
             //List<DataEntry> list = new List<DataEntry>();
             //dataGridView1.Rows.Add(ar);
-            /*
+            
             List<string> list = new List<string>();
             string[] ar = { ot1.Text, ot2.Text, ot3.Text, ot4.Text };
             list.AddRange(ar);
